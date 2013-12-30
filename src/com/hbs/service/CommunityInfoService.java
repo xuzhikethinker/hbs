339c675ffe62d0160081aa845dcd3ece9b204722
xuzh <zhike.xu@cargosmart.com>
2013-12-30 13:25:21 +0800

    update code

---
+++ src/com/hbs/service/CommunityInfoService.java
@@ -0,0 +1,7 @@
+package com.hbs.service;
+
+/**
+ * Created by XUZH on 12/30/13.
+ */
+public interface CommunityInfoService {
+}
--- src/com/hbs/webapp/view/BaseView.java
+++ src/com/hbs/webapp/view/BaseView.java
@@ -7,26 +7,32 @@ package com.hbs.webapp.view;
 
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
+import javax.servlet.ServletContext;
 
 /**
- *
+ * 
  * @author develop
  */
 public abstract class BaseView {
 
-    protected FacesContext getCurrentFacesContext() {
-        return FacesContext.getCurrentInstance();
-    }
+  protected FacesContext getCurrentFacesContext() {
+    return FacesContext.getCurrentInstance();
+  }
 
-    /**
-     * 可以通过ExternalContext获取requestMap等，getRequestParameterMap()
-     * @return 
-     */
-    protected ExternalContext getCurrentExternalContext() {
-        return this.getCurrentFacesContext().getExternalContext();
-    }
-    
-    protected String getInitParamValueByKey(String name){
-        return this.getCurrentExternalContext().getInitParameter(name);
-    }
+  /**
+   * 可以通过ExternalContext获取requestMap等，getRequestParameterMap()
+   * 
+   * @return
+   */
+  protected ExternalContext getCurrentExternalContext() {
+    return this.getCurrentFacesContext().getExternalContext();
+  }
+
+  protected ServletContext getServletContext() {
+    return (ServletContext) this.getCurrentExternalContext().getContext();
+  }
+
+  protected String getInitParamValueByKey(String name) {
+    return this.getCurrentExternalContext().getInitParameter(name);
+  }
 }
